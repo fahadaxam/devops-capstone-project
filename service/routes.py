a36a4c1b1afc99a52bf6cf75122dd34774a9cf69
fahadaxam <fahadazam@outlook.com>
2024-02-15 10:15:28 -0500

    add some blanks

--- service/routes.py
+++ service/routes.py
@@ -57,11 +57,11 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
-
 def list_accounts():
     """
     List all Accounts
@@ -75,11 +75,11 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
 def get_accounts(account_id):
     """
     Reads an Account
@@ -112,11 +112,11 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
 def delete_accounts(account_id):
     """
     Delete an Account
@@ -129,6 +129,7 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
