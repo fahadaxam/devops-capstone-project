3af4af43d64180fd792a9969d3ce38aec7ff4681
fahadaxam <fahadazam@outlook.com>
2024-02-16 12:14:29 -0500

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -8,9 +8,11 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
+talisman = Talisman(app)
 app.config.from_object(config)
 
 # Import the routes After the Flask app is created
